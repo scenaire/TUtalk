99315bed85cb663bc6e23afb2d8379d0e3297506
ACER <ACER@172.25.114.135>
2018-11-23 16:28:25 +0700

    เอา ip,port ออก

--- Chat/src/GUI/MainFrame.java
+++ Chat/src/GUI/MainFrame.java
@@ -7,13 +7,13 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame{
 	
 	public MainFrame() {
-		MainPanel p = new MainPanel();
+	/*MainPanel p = new MainPanel();
 		this.add(p);
 		this.setSize(p.getWidth(), p.getHeight());
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width  - getSize().width) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - getSize().height) / 2);
-
+*/
 	}
 
 }
